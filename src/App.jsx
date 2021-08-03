90550894d9b26abfcab0ab9c45f3a0e4f62125c9
BennyZHill <ben.h@builtbypixel.com>
2021-08-03 15:17:16 +0100

    backdrop filter blur

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 
   const handleFetch = async () => {
     try {
-      const response = await fetch('https://icanhazdadjoke.com/');
+      const response = await fetch('https://api.chucknorris.io/jokes/random');
       if (response.status !== 200) throw new Error(`${response.status}`);
       const data = await response.json();
       setData(data);
@@ -25,10 +25,14 @@ const App = () => {
   if (error) <h1>Error.</h1>;
   else if (loading) <h1>Loading...</h1>;
   return (
-    <div>
-      <h1>{data.value}</h1>
-      <button onClick={handleFetch}>click me</button>
+    <div className = 'joke-container'>
+      <h1>CHUCKS  <span className='title'> CHUCKLES</span></h1>
+      <div>
+      <h2 className='joke'>{data.value}</h2>
+      </div>
+      <button onClick={handleFetch}></button>
     </div>
+    
   );
 };
 
