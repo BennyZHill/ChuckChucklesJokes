7d012b1d44495f36470dbdce74100b0e65ba6e12
BennyZHill <ben.h@builtbypixel.com>
2021-08-04 09:48:51 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ const App = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-
+  
   useEffect(() => {
       handleFetch()
   }, [])
-
   const handleFetch = async () => {
     try {
-      const response = await fetch('https://api.chucknorris.io/jokes/random');
-      if (response.status !== 200) throw new Error(`${response.status}`);
-      const data = await response.json();
+      const res = await fetch('https://api.chucknorris.io/jokes/random');
+      if (res.status !== 200) throw new Error(`${res.status}`);
+      const data = await res.json();
       setData(data);
       setLoading(false);
     } catch (error) {
@@ -26,8 +25,9 @@ const App = () => {
   else if (loading) <h1>Loading...</h1>;
   return (
     <div className = 'joke-container'>
-      <h1>CHUCKS  <span className='title'> CHUCKLES</span></h1>
+      <h1>CHUCKS<span className='title'> CHUCKLES</span></h1>
       <div>
+      <div className='icon'></div>
       <h2 className='joke'>{data.value}</h2>
       </div>
       <button onClick={handleFetch}></button>
